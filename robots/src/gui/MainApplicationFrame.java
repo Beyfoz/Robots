63c65f27dca21406464e201d34d63911fe8670cc
Ярослав <yaroslav6467@gmail.com>
2025-03-11 09:30:23 +0500

    обратно вернул к систем экзит

--- robots/src/gui/MainApplicationFrame.java
+++ robots/src/gui/MainApplicationFrame.java
@@ -1,19 +1,16 @@
 package gui;
 
 import java.awt.*;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.beans.PropertyChangeListener;
-import java.beans.PropertyChangeSupport;
 import javax.swing.*;
 import javax.swing.event.InternalFrameAdapter;
 import javax.swing.event.InternalFrameEvent;
-
 import log.Logger;
 
 public class MainApplicationFrame extends JFrame {
     private final JDesktopPane desktopPane = new JDesktopPane();
-    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
 
     public MainApplicationFrame() {
         int inset = 50;
@@ -31,19 +28,16 @@ public class MainApplicationFrame extends JFrame {
         gameWindow.setSize(400, 400);
         addWindow(gameWindow);
 
-        setJMenuBar(new MenuBarFactory(this).createMenuBar());
+        setJMenuBar(generateMenuBar());
         setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
         addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent windowEvent) {
                 if (confirmAndExit()) {
-                    fireApplicationExitEvent();
+                    closeAllInternalFrames();
+                    System.exit(0);
                 }
             }
-        });
-
-        addApplicationExitListener(evt -> {
-            dispose();
         });
     }
 
@@ -64,13 +58,64 @@ public class MainApplicationFrame extends JFrame {
         frame.addInternalFrameListener(new InternalFrameAdapter() {
             @Override
             public void internalFrameClosing(InternalFrameEvent e) {
-                if (!confirmAndExit()) {
-                    e.getInternalFrame().setDefaultCloseOperation(JInternalFrame.DO_NOTHING_ON_CLOSE);
+                if (confirmInternalFrameClose(e.getInternalFrame())) {
+                    e.getInternalFrame().dispose(); // Закрываем только это окно
                 } else {
-                    e.getInternalFrame().setDefaultCloseOperation(JInternalFrame.DISPOSE_ON_CLOSE);
+                    e.getInternalFrame().setDefaultCloseOperation(JInternalFrame.DO_NOTHING_ON_CLOSE);
                 }
             }
         });
+    }
+
+    private JMenuBar generateMenuBar() {
+        JMenuBar menuBar = new JMenuBar();
+
+        JMenu fileMenu = new JMenu("Файл");
+        fileMenu.setMnemonic(KeyEvent.VK_F);
+
+        JMenuItem exitMenuItem = new JMenuItem("Выход", KeyEvent.VK_X);
+        exitMenuItem.addActionListener((event) -> {
+            if (confirmAndExit()) {
+                closeAllInternalFrames();
+                System.exit(0);
+            }
+        });
+        fileMenu.add(exitMenuItem);
+
+        JMenu lookAndFeelMenu = new JMenu("Режим отображения");
+        lookAndFeelMenu.setMnemonic(KeyEvent.VK_V);
+        lookAndFeelMenu.getAccessibleContext().setAccessibleDescription(
+                "Управление режимом отображения приложения");
+
+        JMenuItem systemLookAndFeel = new JMenuItem("Системная схема", KeyEvent.VK_S);
+        systemLookAndFeel.addActionListener((event) -> {
+            setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            this.invalidate();
+        });
+        lookAndFeelMenu.add(systemLookAndFeel);
+
+        JMenuItem crossplatformLookAndFeel = new JMenuItem("Универсальная схема", KeyEvent.VK_U);
+        crossplatformLookAndFeel.addActionListener((event) -> {
+            setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+            this.invalidate();
+        });
+        lookAndFeelMenu.add(crossplatformLookAndFeel);
+
+        JMenu testMenu = new JMenu("Тесты");
+        testMenu.setMnemonic(KeyEvent.VK_T);
+        testMenu.getAccessibleContext().setAccessibleDescription(
+                "Тестовые команды");
+
+        JMenuItem addLogMessageItem = new JMenuItem("Сообщение в лог", KeyEvent.VK_S);
+        addLogMessageItem.addActionListener((event) -> {
+            Logger.debug("Новая строка");
+        });
+        testMenu.add(addLogMessageItem);
+
+        menuBar.add(fileMenu);
+        menuBar.add(lookAndFeelMenu);
+        menuBar.add(testMenu);
+        return menuBar;
     }
 
     public void setLookAndFeel(String className) {
@@ -79,6 +124,7 @@ public class MainApplicationFrame extends JFrame {
             SwingUtilities.updateComponentTreeUI(this);
         } catch (ClassNotFoundException | InstantiationException
                  | IllegalAccessException | UnsupportedLookAndFeelException e) {
+            // just ignore
         }
     }
 
@@ -97,15 +143,25 @@ public class MainApplicationFrame extends JFrame {
         return option == JOptionPane.YES_OPTION;
     }
 
-    private void fireApplicationExitEvent() {
-        support.firePropertyChange("ApplicationExit", false, true);
+    private boolean confirmInternalFrameClose(JInternalFrame frame) {
+        int option = JOptionPane.showOptionDialog(
+                this,
+                "Вы уверены, что хотите закрыть это окно?",
+                "Подтверждение закрытия окна",
+                JOptionPane.YES_NO_OPTION,
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                new Object[]{"Да", "Нет"},
+                "Нет"
+        );
+
+        return option == JOptionPane.YES_OPTION;
     }
 
-    public void addApplicationExitListener(PropertyChangeListener listener) {
-        support.addPropertyChangeListener("ApplicationExit", listener);
-    }
-
-    public void removeApplicationExitListener(PropertyChangeListener listener) {
-        support.removePropertyChangeListener("ApplicationExit", listener);
+    private void closeAllInternalFrames() {
+        JInternalFrame[] frames = desktopPane.getAllFrames();
+        for (JInternalFrame frame : frames) {
+            frame.dispose();
+        }
     }
 }
