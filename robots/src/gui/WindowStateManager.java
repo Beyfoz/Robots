38231a766a9726bc7d3c537d372f67d68e25fd2e
Яр <yaroslav6467@gmail.com>
2025-03-23 17:00:53 +0500

    поменял id на имена

--- robots/src/gui/WindowStateManager.java
+++ robots/src/gui/WindowStateManager.java
@@ -2,7 +2,6 @@ package gui;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
-
 import javax.swing.*;
 import java.awt.*;
 import java.io.FileWriter;
@@ -19,18 +18,22 @@ public class WindowStateManager {
         Map<String, WindowState> windowStates = new HashMap<>();
 
         for (JInternalFrame frame : desktopPane.getAllFrames()) {
-            String windowId = frame.getTitle();
+            String windowName = frame.getName();
+            if (windowName == null) {
+                System.err.println("Окно без имени: " + frame.getTitle());
+                continue;
+            }
             Rectangle bounds = frame.getBounds();
             boolean isIcon = frame.isIcon();
 
-            windowStates.put(windowId, new WindowState(windowId, bounds, isIcon));
+            windowStates.put(windowName, new WindowState(windowName, bounds, isIcon));
         }
 
         try (FileWriter writer = new FileWriter(CONFIG_FILE)) {
             JSONArray jsonArray = new JSONArray();
             for (WindowState state : windowStates.values()) {
                 JSONObject jsonObject = new JSONObject();
-                jsonObject.put("windowId", state.getWindowId());
+                jsonObject.put("windowName", state.getWindowName());
                 jsonObject.put("x", state.getBounds().x);
                 jsonObject.put("y", state.getBounds().y);
                 jsonObject.put("width", state.getBounds().width);
@@ -55,7 +58,7 @@ public class WindowStateManager {
 
             for (int i = 0; i < jsonArray.length(); i++) {
                 JSONObject jsonObject = jsonArray.getJSONObject(i);
-                String windowId = jsonObject.getString("windowId");
+                String windowName = jsonObject.getString("windowName");
                 int x = jsonObject.getInt("x");
                 int y = jsonObject.getInt("y");
                 int width = jsonObject.getInt("width");
@@ -63,7 +66,7 @@ public class WindowStateManager {
                 boolean isIcon = jsonObject.getBoolean("isIcon");
 
                 for (JInternalFrame frame : desktopPane.getAllFrames()) {
-                    if (frame.getTitle().equals(windowId)) {
+                    if (windowName.equals(frame.getName())) {
                         frame.setBounds(x, y, width, height);
                         try {
                             if (isIcon) {
