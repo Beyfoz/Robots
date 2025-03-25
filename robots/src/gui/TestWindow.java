13a491940100d239f368b7ddf73be661cccca96c
NIX-EK <yaroslav6467@gmail.com>
2025-03-25 13:53:44 +0500

    убрал лишний setSize

--- robots/src/gui/TestWindow.java
+++ robots/src/gui/TestWindow.java
@@ -6,6 +6,5 @@ public class TestWindow extends JInternalFrame {
     public TestWindow() {
         super("Тестовое окно для сериализации", true, true, true, true);
         setName("TestWindow");
-        setSize(40, 40);
     }
 }
