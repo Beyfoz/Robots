c311bac9efc164776b04a3c0a00a586771016071
NIX-EK <yaroslav6467@gmail.com>
2025-03-25 13:51:42 +0500

    сделал тест окно поменьше

--- robots/src/gui/TestWindow.java
+++ robots/src/gui/TestWindow.java
@@ -6,6 +6,6 @@ public class TestWindow extends JInternalFrame {
     public TestWindow() {
         super("Тестовое окно для сериализации", true, true, true, true);
         setName("TestWindow");
-        setSize(300, 200);
+        setSize(40, 40);
     }
 }
