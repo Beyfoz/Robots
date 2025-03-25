271830f3f1abca5b50c12e9a65c0614d1325fe3d
NIX-EK <yaroslav6467@gmail.com>
2025-03-25 08:21:53 +0300

    чистка кода

--- robots/src/gui/WindowState.java
+++ robots/src/gui/WindowState.java
@@ -4,8 +4,6 @@ import java.awt.Rectangle;
 import java.io.Serializable;
 
 public class WindowState implements Serializable {
-    private static final long serialVersionUID = 1L;
-
     private String windowName;
     private Rectangle bounds;
     private boolean isCollapsed;
